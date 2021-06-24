c3fcc2f547dbb5a3a03137d09989c42a3fdd401b
Ankit6694 <ankit93jain@gmail.com>
2021-06-24 14:51:18 +0530

    Add files via upload

---
+++ src/main/java/com/example/FirstBoot/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
